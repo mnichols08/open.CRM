a78abf33c712547d302e9436190a96a617b61a4c
Mikey Nichols <mnix@journeytocode.io>
2024-07-09 19:49:53 -0400

    fix deploy errors

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -77,7 +77,7 @@ import {
   TooltipProvider
 } from "@/components/ui/tooltip"
 
-export default function Dashboard() {
+export default function Page() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
