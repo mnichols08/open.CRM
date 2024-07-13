87e7e54df8d9d2e7b48f82af05482389f407ed94
Mikey Nichols <mnix@journeytocode.io>
2024-07-13 19:48:39 -0400

    creates a new component for "top row" adding in some random variables to set the header up

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -46,48 +46,23 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import Summary from "@/components/Summary";
-
+import TopRow from "@/components/Dashboard/TopRow";
 
 export default function Dashboard() {
+  // Example values for the tickets dashboard (top row variables)
+  const wTickets = Math.floor(Math.random() * 100);
+  const wTicketsLastWeek = Math.floor(Math.random() * 100);
+  const mTickets = wTicketsLastWeek * 4;
+  const mTicketsLastMonth = Math.floor(Math.random() * 1000);
   return (
     <>
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-          <Summary
-            title="Tickets"
-            desc="Introducing Our Dynamic Tickets Dashboard for Seamless Management and Insightful Analysis."
-            buttonText={"Create New Ticket"}
-          />
-          <Card x-chunk="dashboard-05-chunk-1">
-            <CardHeader className="pb-2">
-              <CardDescription>This Week</CardDescription>
-              <CardTitle className="text-4xl">$1,329</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="text-xs text-muted-foreground">
-                +25% from last week
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Progress value={25} aria-label="25% increase" />
-            </CardFooter>
-          </Card>
-          <Card x-chunk="dashboard-05-chunk-2">
-            <CardHeader className="pb-2">
-              <CardDescription>This Month</CardDescription>
-              <CardTitle className="text-4xl">$5,329</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="text-xs text-muted-foreground">
-                +10% from last month
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Progress value={12} aria-label="12% increase" />
-            </CardFooter>
-          </Card>
-        </div>
+        <TopRow
+          wTickets={wTickets}
+          wTicketsLastWeek={wTicketsLastWeek}
+          mTickets={mTickets}
+          mTicketsLastMonth={mTicketsLastMonth}
+        />
         <Tabs defaultValue="week">
           <div className="flex items-center">
             <TabsList>
