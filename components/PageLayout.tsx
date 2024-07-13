0bec49c4e08af6a573f658297258c007c71714c9
Mikey Nichols <mnix@journeytocode.io>
2024-07-13 18:50:26 -0400

    creates a recyclable page layout

---
+++ components/PageLayout.tsx
@@ -0,0 +1,33 @@
+import Aside from "@/components/Aside";
+import Header from "@/components/Header";
+
+function OuterWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      {children}
+    </div>
+  );
+}
+function InnerWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+      <Header />
+      <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default function PageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <OuterWrapper>
+      <Aside />
+      <InnerWrapper>{children}</InnerWrapper>
+    </OuterWrapper>
+  );
+}
