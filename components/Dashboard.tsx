f7971eb6d1bfb7c6565e7f2b872a5a327440e0fc
Mikey Nichols <mnix@journeytocode.io>
2024-07-13 20:02:47 -0400

    moves main into dashboard again

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -55,7 +55,7 @@ export default function Dashboard() {
   const mTickets = wTicketsLastWeek * 4;
   const mTicketsLastMonth = Math.floor(Math.random() * 1000);
   return (
-    <>
+          <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
         <TopRow
           wTickets={wTickets}
@@ -444,6 +444,6 @@ export default function Dashboard() {
           </CardFooter>
         </Card>
       </div>
-    </>
+    </main>
   );
 }
