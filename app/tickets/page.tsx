05d2a2938a7807fd3de827dc12dd8d26889a00c2
Mikey Nichols <mnix@journeytocode.io>
2024-07-14 11:42:14 -0400

    uses AllTickets component

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { Search, PlusCircle } from "lucide-react";
-import { Input } from "@/components/ui/input";
 import TopRow from "@/components/Dashboard/TopRow";
 import { Separator } from "@/components/ui/separator";
 import {
@@ -9,7 +7,6 @@ import {
   Copy,
   File,
   ListFilter,
-  MoreHorizontal,
   MoreVertical,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -35,17 +32,10 @@ import {
   PaginationContent,
   PaginationItem,
 } from "@/components/ui/pagination";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { sql } from "@vercel/postgres";
-import { Ticket } from "../lib/definitions";
+import AllTickets from "@/components/Tickets/AllTickets";
+
 
 export default async function TicketsDashboard() {
   // Example values for the tickets dashboard (top row variables)
@@ -53,7 +43,6 @@ export default async function TicketsDashboard() {
   const wTicketsLastWeek = Math.floor(Math.random() * 100);
   const mTickets = wTicketsLastWeek * 4;
   const mTicketsLastMonth = Math.floor(Math.random() * 1000);
-  const data = await sql<Ticket>`SELECT * FROM tickets`;
   const exampleTicket = {
     customer: "RAPID REPAIR",
     customerID: "2afa1b1a-4525-45c1-af03-cd191a3efd04",
@@ -112,116 +101,7 @@ export default async function TicketsDashboard() {
             </div>
           </div>
           <TabsContent value="week">
-            <Card x-chunk="dashboard-05-chunk-3">
-              <CardHeader className="px-7">
-                <CardTitle>Tickets</CardTitle>
-                <CardDescription>Recent tickets or inquiry.</CardDescription>
-              </CardHeader>
-              <CardContent>
-              <div className="flex items-center justify-between">
-              <div className="relative flex-1 md:flex-none">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                  type="search"
-                  placeholder="Search for a ticket"
-                  className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-                />
-              </div>
-              <div className="flex items-center gap-2">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="outline" size="sm" className="h-8 gap-1">
-                      <ListFilter className="h-3.5 w-3.5" />
-                      <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                        Filter
-                      </span>
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuCheckboxItem checked>
-                      Active
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>
-                      Archived
-                    </DropdownMenuCheckboxItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-                <Button size="sm" variant="outline" className="h-8 gap-1">
-                  <File className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Export
-                  </span>
-                </Button>
-                <Button size="sm" className="h-8 gap-1">
-                  <PlusCircle className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Add Ticket
-                  </span>
-                </Button>
-              </div>
-            </div>
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Id</TableHead>
-                      <TableHead>
-                        <span className="sr-only">Reason</span>
-                      </TableHead>
-                      <TableHead>Status</TableHead>
-                      <TableHead>Year</TableHead>
-                      <TableHead>Make</TableHead>
-                      <TableHead>Model</TableHead>
-                      <TableHead>Engine</TableHead>
-                      <TableHead>Submodel</TableHead>
-                      <TableHead>Created By </TableHead>
-                      <TableHead>Customer Id</TableHead>
-                      <TableHead>
-                        <span className="sr-only">Actions</span>
-                      </TableHead>
-                    </TableRow>
-                  </TableHeader>
-
-                  <TableBody>
-                    {data.rows.map((ticket) => (
-                      <TableRow key={ticket.id}>
-                        <TableCell>{ticket.id}</TableCell>
-                        <TableCell>{ticket.reason}</TableCell>
-                        <TableCell>{ticket.status}</TableCell>
-                        <TableCell>{ticket.year}</TableCell>
-                        <TableCell>{ticket.make}</TableCell>
-                        <TableCell>{ticket.model}</TableCell>
-                        <TableCell>{ticket.engine}</TableCell>
-                        <TableCell>{ticket.submodel}</TableCell>
-                        <TableCell>{ticket.created_by}</TableCell>
-                        <TableCell>{ticket.customer_id}</TableCell>
-                        <TableCell>
-                          <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                              <Button
-                                aria-haspopup="true"
-                                size="icon"
-                                variant="ghost"
-                              >
-                                <MoreHorizontal className="h-4 w-4" />
-                                <span className="sr-only">Toggle menu</span>
-                              </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent align="end">
-                              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                              <Link href="/tickets/edit"><DropdownMenuItem>Edit</DropdownMenuItem></Link>
-                              <DropdownMenuItem>Delete</DropdownMenuItem>
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </CardContent>
-            </Card>
+            <AllTickets />
           </TabsContent>
         </Tabs>
       </div>
@@ -257,7 +137,9 @@ export default async function TicketsDashboard() {
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
-                  <Link href="/tickets/edit"><DropdownMenuItem>Edit</DropdownMenuItem></Link>
+                  <Link href="/tickets/edit">
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                  </Link>
                   <DropdownMenuItem>Export</DropdownMenuItem>
                   <DropdownMenuSeparator />
                   <DropdownMenuItem>Trash</DropdownMenuItem>
