bcb7a8e4aeca5cc42291c7625627d71b59bddeb3
Mikey Nichols <mnix@journeytocode.io>
2024-07-13 23:59:59 -0400

    removes arguments passed in for breadcrumbs

--- app/tickets/create/page.tsx
+++ app/tickets/create/page.tsx
@@ -26,7 +26,7 @@ import { Textarea } from "@/components/ui/textarea";
 
 export default function CreateTicketPage() {
   return (
-    <PageLayout breadcrumb={["Tickets", "Create Ticket"]}>
+    <PageLayout>
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
         <div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
           <div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
