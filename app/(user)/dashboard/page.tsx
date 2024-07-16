7b671c91b38739e067b71facd805446080dcf6a7
Mikey Nichols <mnix@journeytocode.io>
2024-07-16 16:52:43 -0400

    builds a user dashboard a little better

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -35,6 +35,9 @@ import {
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import AllTickets from "@/components/Tickets/AllTickets";
+import AllCustomers from "@/components/Customers/AllCustomers";
+import AllOrders from "@/components/Orders/AllOrders";
+import AllProducts from "@/components/Products/AllProducts";
 
 export default async function TicketsDashboard() {
   // Example values for the tickets dashboard (top row variables)
@@ -70,43 +73,26 @@ export default async function TicketsDashboard() {
           Card2={{title: "Orders", description: "", content: "", progress: nTickets}}
   
         />
-        <Tabs defaultValue="week">
+        <Tabs defaultValue="tickets">
           <div className="flex items-center">
             <TabsList>
-              <TabsTrigger value="week">Week</TabsTrigger>
-              <TabsTrigger value="month">Month</TabsTrigger>
-              <TabsTrigger value="year">Year</TabsTrigger>
+              <TabsTrigger value="tickets">Tickets</TabsTrigger>
+              <TabsTrigger value="customers">Customers</TabsTrigger>
+              <TabsTrigger value="orders">Orders</TabsTrigger>
+              <TabsTrigger value="products">Products</TabsTrigger>
             </TabsList>
-            <div className="ml-auto flex items-center gap-2">
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="outline"
-                    size="sm"
-                    className="h-7 gap-1 text-sm"
-                  >
-                    <ListFilter className="h-3.5 w-3.5" />
-                    <span className="sr-only sm:not-sr-only">Filter</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuCheckboxItem checked>
-                    Fulfilled
-                  </DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem>Declined</DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem>Refunded</DropdownMenuCheckboxItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-              <Button size="sm" variant="outline" className="h-7 gap-1 text-sm">
-                <File className="h-3.5 w-3.5" />
-                <span className="sr-only sm:not-sr-only">Export</span>
-              </Button>
-            </div>
           </div>
-          <TabsContent value="week">
+          <TabsContent value="tickets">
             <AllTickets />
+          </TabsContent>
+          <TabsContent value="customers">
+            <AllCustomers />
+          </TabsContent>
+          <TabsContent value="orders">
+            <AllOrders />
+          </TabsContent>
+          <TabsContent value="products">
+            <AllProducts />
           </TabsContent>
         </Tabs>
       </div>
