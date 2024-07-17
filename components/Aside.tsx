e0ebe830288b6b264070380572798bdd8bd3d412
Mikey Nichols <mnix@journeytocode.io>
2024-07-16 23:59:48 -0400

    updates aside links and icons

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -1,9 +1,8 @@
 import Link from "next/link";
 import {
     Home,
-    LineChart,
     Package,
-    Package2,
+    Ticket,
     Settings,
     ShoppingCart,
     Users2,
@@ -19,18 +18,11 @@ export default function Aside() {
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
     <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-      <Link
-        href="#"
-        className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-      >
-        <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-        <span className="sr-only">Acme Inc</span>
-      </Link>
       <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
           <Link
-            href="#"
+            href="/dashboard"
             className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
           >
             <Home className="h-5 w-5" />
@@ -42,19 +34,32 @@ export default function Aside() {
       <Tooltip>
         <TooltipTrigger asChild>
           <Link
-            href="#"
+            href="/customers"
             className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
           >
-            <ShoppingCart className="h-5 w-5" />
-            <span className="sr-only">Orders</span>
+            <Users2 className="h-5 w-5" />
+            <span className="sr-only">Customers</span>
           </Link>
         </TooltipTrigger>
-        <TooltipContent side="right">Orders</TooltipContent>
+        <TooltipContent side="right">Customers</TooltipContent>
+      </Tooltip>
+ 
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link
+            href="/tickets"
+            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+          >
+            <Ticket className="h-5 w-5" />
+            <span className="sr-only">Tickets</span>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent side="right">Tickets</TooltipContent>
       </Tooltip>
       <Tooltip>
         <TooltipTrigger asChild>
           <Link
-            href="#"
+            href="/products"
             className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
           >
             <Package className="h-5 w-5" />
@@ -66,26 +71,14 @@ export default function Aside() {
       <Tooltip>
         <TooltipTrigger asChild>
           <Link
-            href="#"
+            href="/orders"
             className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
           >
-            <Users2 className="h-5 w-5" />
-            <span className="sr-only">Customers</span>
+            <ShoppingCart className="h-5 w-5" />
+            <span className="sr-only">Orders</span>
           </Link>
         </TooltipTrigger>
-        <TooltipContent side="right">Customers</TooltipContent>
-      </Tooltip>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link
-            href="#"
-            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-          >
-            <LineChart className="h-5 w-5" />
-            <span className="sr-only">Analytics</span>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">Analytics</TooltipContent>
+        <TooltipContent side="right">Orders</TooltipContent>
       </Tooltip>
       </TooltipProvider>
     </nav>
@@ -94,7 +87,7 @@ export default function Aside() {
       <Tooltip>
         <TooltipTrigger asChild>
           <Link
-            href="#"
+            href="/settings"
             className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
           >
             <Settings className="h-5 w-5" />
