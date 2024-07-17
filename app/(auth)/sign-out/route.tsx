37f2d47b41f50756361fc209183febb50be18f69
Mikey Nichols <mnix@journeytocode.io>
2024-07-16 21:06:27 -0400

    fixes the sign out problem

--- app/(auth)/sign-out/route.tsx
+++ app/(auth)/sign-out/route.tsx
@@ -2,5 +2,15 @@ import { signOut } from "@/auth";
 
 export async function GET(req: any, res: any) {
   const signedOut = await signOut();
-  return signedOut ? res.status(200).end() : res.status(400).end();
+  if (signedOut) {
+    res.writeHead(302, {
+      Location: "/",
+    });
+    res.end();
+  } else {
+    res.writeHead(500, {
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify({ error: "An unexpected error occurred" }));
+  } 
 }
