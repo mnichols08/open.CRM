ee0d21b6f32f67485d50c359baf074218dfb1eee
Mikey Nichols <mnix@journeytocode.io>
2024-07-17 23:19:29 -0400

    sets debug to true

--- auth.config.ts
+++ auth.config.ts
@@ -1,17 +1,15 @@
 import type { NextAuthConfig } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import type { User } from "@/lib/definitions";
 export const authConfig = {
   trustHost: true,
   pages: {
     signIn: "/",
     signOut: "/sign-out",
   },
+  debug: true,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      // const user: User = auth?.user;
       const user: any = auth?.user;
-
       const isLoggedIn = !!auth?.user;
       const isOnLandingPage = nextUrl.pathname === "/";
       const isOnSignupPage = nextUrl.pathname.startsWith("/sign-up");
@@ -19,12 +17,6 @@ export const authConfig = {
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnLoginPage || isOnLandingPage || isOnSignupPage) {
         if (isLoggedIn) {
-          console.log("User is logged in", auth);
-          // if (user.is_admin) {
-          //   return Response.redirect(new URL("/admin", nextUrl));
-          // } else {
-          //   return Response.redirect(new URL("/dashboard", nextUrl));
-          // }
           return Response.redirect(new URL("/dashboard", nextUrl)); // Redirect authenticated users to dashboard
         }
         return true;
