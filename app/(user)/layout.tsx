3f367ccf9433d7064693943600894320dd5e67a5
Mikey Nichols <mnix@journeytocode.io>
2024-07-17 22:27:32 -0400

    checks if user is signed in before allowing them to use app

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,7 +1,15 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 import PageLayout from "@/components/PageLayout";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <PageLayout>{children}</PageLayout>
-  );
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+
+  if (!session) return redirect("/");
+
+  return <PageLayout>{children}</PageLayout>;
 }
