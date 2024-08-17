6b71edac153c260aa8b8055032161b18c0f2fbc1
Jayanti Hari <lnu.jay@northeastern.edu>
2024-08-16 22:50:05 -0400

    customer drop down changed to combo box

--- components/CustomerDropDown.tsx
+++ components/CustomerDropDown.tsx
@@ -1,33 +1,81 @@
-import { db } from "@vercel/postgres";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+"use client";
+import { useState, useEffect } from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
 
-export default async function CustomerDropDown({
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { fetchCustomers } from "@/lib/actions";
+import { Customer } from "@/lib/definitions";
+
+export default function CustomerDropDown({
   selectedCustomer,
 }: {
   selectedCustomer: string | undefined;
 }) {
-  const client = await db.connect();
-  const customerQuery = await client.query("SELECT * FROM customers");
-  const customers = customerQuery.rows;
-  client.release();
+  const [customers, setCustomers] = useState<Customer[]>([]);
+  useEffect(() => {
+    fetchCustomers().then((customers) => setCustomers(customers));
+  }, []);
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(selectedCustomer);
+
   return (
-    <Select name="customer" defaultValue={selectedCustomer} required>
-      <SelectTrigger id="customer" aria-label="Select Customer">
-        <SelectValue placeholder="Select Customer" />
-      </SelectTrigger>
-      <SelectContent>
-        {customers.map((customer) => (
-          <SelectItem key={customer.id} value={customer.id}>
-            {customer.name}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[300px] justify-between"
+        >
+          {value
+            ? customers.find((customers) => customers.name === value)?.name
+            : "Select customer..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search customer..." />
+          <CommandList>
+            <CommandEmpty>No customer found.</CommandEmpty>
+            <CommandGroup>
+              {customers.map((customer) => (
+                <CommandItem
+                  key={customer.name}
+                  value={customer.name}
+                  onSelect={(currentValue: any) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === customer.name ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {customer.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
   );
 }
